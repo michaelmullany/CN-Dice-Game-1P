ce044894302e21b54702b972da068696da2bef45
Michael Mullany <mamullany@gmail.com>
2022-03-31 15:09:26 +0100

    Fix score display style

--- app.js
+++ app.js
@@ -4,13 +4,10 @@ let targetScore = 20;
 let illegalValue = 1;
 
 const rollButton = document.getElementById("rollButton");
-const currentScoreDisplay = document.getElementById("currentScore");
+const currentScoreDisplay = document.getElementById("scoreDisplay");
 const targetScoreDisplay = document.getElementById("targetScore");
 const diceImage = document.getElementById("diceImage");
 const instructionText = document.getElementById("instruction");
-
-// Set a variable for the target score so it can be changed easily
-targetScoreDisplay.textContent = targetScore;
 
 rollButton.addEventListener("click", () => {
     rollDice();
@@ -47,7 +44,6 @@ const updateScoreDisplay = () => {
 
 const processLoss = () => {
     currentScore = 0;
-    updateScoreDisplay();
     instructionText.textContent = "You Lost! Roll the dice to start a new game.";
 };
 
@@ -60,4 +56,3 @@ const setDiceImage = () => {
     diceImage.style.display = "inline";
     diceImage.src=`img/dice${diceValue}.png`;
 }
-
