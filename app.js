4abf478a0242017dda418b5f3e55757cf71903ab
Michael Mullany <mamullany@gmail.com>
2022-03-31 10:47:53 +0100

    Add target score display

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ let targetScore = 20;
 
 const rollButton = document.getElementById("rollButton");
 const currentScoreDisplay = document.getElementById("currentScore");
+const targetScoreDisplay = document.getElementById("targetScore");
+
+// Set a variable for the target score so it can be changed easily
+targetScoreDisplay.textContent = targetScore;
 
 rollButton.addEventListener("click", () => {
     diceValue = Math.ceil(Math.random() * 6);
